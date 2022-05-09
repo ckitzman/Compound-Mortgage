7004573dbfdf1829cbe43285026d368672de1a9c
Caleb Kitzman <71990906+ckitzman@users.noreply.github.com>
2022-05-09 17:07:47 -0500

    added .py

---
+++ mortgage_calculator.py
@@ -0,0 +1,9 @@
+#Calculate your mortgage over a designated period of time
+
+principal = #initial investment
+interestRate = #interestrate 
+years = #years 
+print(f'Value of Principal of ${principal} at {(interestRate100):.2f}% interest rate') 
+for i in range(years + 1):
+    print(f'... after {i} years: ${principal:.2f}')
+    principal += principalinterestRate
